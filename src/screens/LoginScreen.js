dd341af8c1088739df6bb6c8ce7c54fb9a5b4a9b
Alex Tan <alextan785@gmail.com>
2019-12-16 18:02:35 -0800

    keyboard listeners for login

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -13,14 +13,26 @@ import {
 import FloatingInput from '../components/general/FloatingInput';
 import CustomButton from '../components/general/CustomButton';
 
-const phone = ''
+// TODO: Display only phone, password, login, and signup when keyboard is open
+// https://stackoverflow.com/questions/51606099/how-to-detect-when-keyboard-is-opened-or-closed-in-react-native
 export default class LoginScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {phone: '', password: ''};
+    this.state = {phone: '', password: '', keyboardOpen: false};
   }
 
+  componentDidMount() {
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this.keyboardDidShow);
+    this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this.keyboardDidHide);
+  }
   
+  keyboardDidShow = () => {
+    this.setState({keyboardOpen: true});
+  }
+
+  keyboardDidHide = () => {
+    this.setState({keyboardOpen: false});
+  }
 
   handleLogin = () => {
     console.log('Trying to Login');
@@ -31,43 +43,63 @@ export default class LoginScreen extends React.Component {
       <ImageBackground
         source={require('../assets/loginGradient.jpg')}
         style={styles.background}>
-        <View style={styles.container}>
-          {/* TODO: Add Image */}
-          <View style={{marginTop: 80}}></View>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          keyboardShouldPersistTaps={'never'}>
 
-          <FloatingInput
-            ref={r => (this.phone = r)}
-            value={this.state.phone}
-            label={'Phone'}
-            onChangeText={text => this.setState({phone: text})}
-            blurOnSubmit={false}
-            onSubmitEditing={() => this.password.getInnerRef().focus()}
-          />
+          <View style={styles.container}>
+            {/* TODO: Add Image */}
+            <View style={{marginTop: 80}}></View>
 
-          <View style={styles.inputDivider}></View>
+            <FloatingInput
+              ref={r => (this.phone = r)}
+              value={this.state.phone}
+              label={'Phone'}
+              labelColorBlur={'#FFFFFF'}
+              onChangeText={text => this.setState({phone: text})}
+              blurOnSubmit={false}
+              onSubmitEditing={() => this.password.getInnerRef().focus()}
+            />
 
-          <FloatingInput
-            ref={r => (this.password = r)}
-            value={this.state.password}
-            label={'Password'}
-            onChangeText={text => this.setState({password: text})}
-            blurOnSubmit={false}
-            onSubmitEditing={this.handleLogin}
-          />
+            <View style={styles.inputDivider}></View>
 
-          <View style={styles.inputDivider}></View>
+            <FloatingInput
+              ref={r => (this.password = r)}
+              value={this.state.password}
+              label={'Password'}
+              labelColorBlur={'#FFFFFF'}
+              onChangeText={text => this.setState({password: text})}
+              blurOnSubmit={false}
+              onSubmitEditing={this.handleLogin}
+            />
 
-          <CustomButton
-            text={'Login'}
-            onPress={this.handleLogin}
-            textStyle={{color: '#000000'}}
-            buttonStyle={{elevation: 10}}
-          />
+            <View style={styles.inputDivider}></View>
 
-          <TouchableOpacity style={{alignItems: 'flex-end'}}>
-            <Text style={styles.clickableText}>Forgot Password?</Text>
+            <CustomButton
+              text={'Login'}
+              onPress={this.handleLogin}
+              textStyle={{color: '#000000'}}
+              buttonStyle={{elevation: 10}}
+            />
+
+            <TouchableOpacity style={{alignItems: 'flex-end'}}>
+              <Text style={styles.clickableText}>Forgot Password?</Text>
+            </TouchableOpacity>
+
+            <View style={{alignItems: 'center', marginVertical: 10}}>
+              <Text style={{fontSize: 24, color: '#FFFFFF'}}>--OR--</Text>
+            </View>
+            
+          </View>
+        </ScrollView>
+
+        <View style={styles.signUpContainer}>
+          <TouchableOpacity onPress={() => console.log('ahh')}>
+            <Text style={{color: 'white'}}>
+              Don't Have An Account?{' '}
+              <Text style={{color: 'white'}}>Sign Up</Text>
+            </Text>
           </TouchableOpacity>
-
         </View>
       </ImageBackground>
     );
@@ -87,6 +119,13 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   clickableText: {
-    color: '#1655F4'
-  }
+    color: 'white',
+  },
+  signUpContainer: {
+    position: 'absolute',
+    bottom: 15,
+    right: 0,
+    left: 0,
+    alignItems: 'center',
+  },
 });
