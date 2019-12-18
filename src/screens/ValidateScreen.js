04dfbb2b0237f5ac08945595b7c516578b48a155
Alex Tan <alextan785@gmail.com>
2019-12-17 17:28:06 -0800

    implement error modal for validation

--- src/screens/ValidateScreen.js
+++ src/screens/ValidateScreen.js
@@ -7,23 +7,28 @@ import {
   ScrollView,
   TouchableWithoutFeedback,
   TouchableOpacity,
-  Platform
+  Platform,
+  PermissionsAndroid
 } from 'react-native';
 
 import SmsListener from 'react-native-android-sms-listener';
-import Header from '../components/general/Header';
+
 import DigitInput from '../components/login/DigitInput';
+import ErrorModal from '../components/login/ErrorModal';
+import Header from '../components/general/Header';
 import CustomButton from '../components/general/CustomButton';
 
 // TODO: Can probably get rid of the editable thing if it is wrapped in touchable without feedback? Fix it?
-// TODO: Make the modal component
 export default class ValidateScreen extends React.Component {
   number_cells = 6;
   constructor(props) {
     super(props);
     this.state = {
-      codes: Array(this.number_cells).fill(''), editable: this.getEditableArray(),
-      username: '', modalVisible: false, error: {}
+      codes: Array(this.number_cells).fill(''),
+      editable: this.getEditableArray(),
+      username: '',
+      showModal: false,
+      error: {},
     };
   }
 
@@ -33,7 +38,7 @@ export default class ValidateScreen extends React.Component {
       editableArray.push(i === 0 ? true : false);
     }
     return editableArray;
-  }
+  };
 
   componentDidMount() {
     if (Platform.OS === 'android') {
@@ -41,7 +46,9 @@ export default class ValidateScreen extends React.Component {
       this.subscription = SmsListener.addListener(message => {
         console.info(message);
         // TODO: Parse the message and set the state, then call on the submit function
-        this.setState({codes: ['1', '2', '3', '4', '5', '6']}, () => console.log('call function here'));
+        this.setState({codes: ['1', '2', '3', '4', '5', '6']}, () =>
+          console.log('call function here'),
+        );
       });
     }
   }
@@ -83,71 +90,82 @@ export default class ValidateScreen extends React.Component {
     }
   }
 
-  selectCursorLocation = (index) => {
+  selectCursorLocation = index => {
     const value = this.state.codes[index];
     if (value === '') {
-      return {start:0, end:0};
+      return {start: 0, end: 0};
     } else {
-      return {start:1, end:1};
+      return {start: 1, end: 1};
     }
-  }
+  };
 
   // TODO: This is not showing the keyboard when you click on any other one.
   focusOnCurrent = () => {
     const code = 'code' + this.state.editable.indexOf(true);
     this.refs[code].focus();
-  }
+  };
 
   // Fires before onChangeText
-  handleKeyPress = ({ nativeEvent: { key: keyValue }}, index) => {
+  handleKeyPress = ({nativeEvent: {key: keyValue}}, index) => {
     if (keyValue === 'Backspace') {
       if (this.state.codes[index] === '') {
         if (index > 0) {
-          const reference = 'code' + (index-1);
-          this.setState((currentState) => {
-            let editArray = currentState.editable;
-            editArray[index] = false;
-            editArray[index - 1] = true;
-            let newCodes = currentState.codes;
-            newCodes[index-1] = '';
-            return {codes: newCodes, editable: editArray};
-          }, () => {
-            this.refs[reference].focus();
-          })
+          const reference = 'code' + (index - 1);
+          this.setState(
+            currentState => {
+              let editArray = currentState.editable;
+              editArray[index] = false;
+              editArray[index - 1] = true;
+              let newCodes = currentState.codes;
+              newCodes[index - 1] = '';
+              return {codes: newCodes, editable: editArray};
+            },
+            () => {
+              this.refs[reference].focus();
+            },
+          );
         }
-      } 
+      }
     }
   };
 
   onChangeText = (index, value) => {
-    this.setState((currentState, currentProps) => {
-      let editArray = currentState.editable;
-      if (index < 5) {
-        editArray[index] = false;
-        editArray[index + 1] = true;
-      }
-      let newCodes = currentState.codes;
-      newCodes[index] = value;
-      return {codes : newCodes, editable: editArray};
-    }, () => {
-      if (value.length === 1) {
+    this.setState(
+      (currentState, currentProps) => {
+        let editArray = currentState.editable;
         if (index < 5) {
-          const reference = 'code' + (index+1);
-          this.refs[reference].focus();
-        } else {
-          // TODO: Keyboard does not persist and im upset
-          this.completeSignup();
+          editArray[index] = false;
+          editArray[index + 1] = true;
         }
-      }
-    })
+        let newCodes = currentState.codes;
+        newCodes[index] = value;
+        return {codes: newCodes, editable: editArray};
+      },
+      () => {
+        if (value.length === 1) {
+          if (index < 5) {
+            const reference = 'code' + (index + 1);
+            this.refs[reference].focus();
+          } else {
+            // TODO: Keyboard does not persist and im upset
+            this.completeSignup();
+          }
+        }
+      },
+    );
   };
 
   completeSignup = () => {
+    this.setState({showModal: true})
     console.log('Sign me up!');
-  }
+  };
 
   handleResend = () => {
     console.log('Resend code!');
+  };
+
+  closeModal = () => {
+    this.setState({showModal: false});
   }
 
   render() {
@@ -159,18 +177,27 @@ export default class ValidateScreen extends React.Component {
           ref={'code' + i}
           value={this.state.codes[i]}
           onChangeText={value => this.onChangeText(i, value)}
-          onKeyPress={(event) => this.handleKeyPress(event, i)}
+          onKeyPress={event => this.handleKeyPress(event, i)}
           selection={this.selectCursorLocation(i)}
           blurOnSubmit={false}
           edit={this.state.editable[i]}
           autoFocus={i === 0 ? true : false}
-        />
+        />,
       );
-    };
+    }
     return (
       <ImageBackground
         source={require('../assets/loginGradient.jpg')}
         style={styles.background}>
+
+        <ErrorModal
+          showModal={this.state.showModal}
+          title={'Wrong Code'}
+          description={'The 6 digit code you entered is incorrect. Try again'}
+          buttonText={'Ok'}
+          closeModal={this.closeModal}
+        />
+
         <ScrollView keyboardShouldPersistTaps={'always'} style={{flex: 1}}>
           <View style={styles.container}>
             <Header
@@ -181,16 +208,17 @@ export default class ValidateScreen extends React.Component {
             {/* Sent to stuff */}
             <View style={{marginTop: 20}}>
               <View style={{}}>
-                <Text style={{fontWeight: 'bold', fontSize: 18}}>Enter the code</Text>
-                <Text style={{fontSize: 16}}>Sent to: {this.props.navigation.getParam('phone', 'default')}</Text>
+                <Text style={{fontWeight: 'bold', fontSize: 18}}>
+                  Enter the code
+                </Text>
+                <Text style={{fontSize: 16}}>
+                  Sent to: {this.props.navigation.getParam('phone', 'default')}
+                </Text>
               </View>
 
               <TouchableWithoutFeedback onPress={this.focusOnCurrent}>
-                <View style={styles.digitContainer}>
-                  {codeInputs}
-                </View>
+                <View style={styles.digitContainer}>{codeInputs}</View>
               </TouchableWithoutFeedback>
-
             </View>
 
             <View style={{marginTop: 20}}>
@@ -201,15 +229,22 @@ export default class ValidateScreen extends React.Component {
                 buttonStyle={{elevation: 10}}
               />
             </View>
-
           </View>
         </ScrollView>
 
-        <TouchableOpacity style={{position: 'absolute', bottom: 10, left: 10}} onPress={this.handleResend}>
-          <Text style={{textAlign: 'center',color: '#007bff',fontStyle: 'italic',fontSize: 16}}>
-            Didn't Get Your Code? Click Here To Resend</Text>
+        <TouchableOpacity
+          style={{position: 'absolute', bottom: 10, left: 10}}
+          onPress={this.handleResend}>
+          <Text
+            style={{
+              textAlign: 'center',
+              color: '#007bff',
+              fontStyle: 'italic',
+              fontSize: 16,
+            }}>
+            Didn't Get Your Code? Click Here To Resend
+          </Text>
         </TouchableOpacity>
-
       </ImageBackground>
     );
   }
@@ -233,5 +268,5 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     marginTop: 40,
-  }
+  },
 });
