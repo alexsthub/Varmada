d2e1ef1ec7465fbb1dd47ee7325f33e020661141
Alex Tan <alextan785@gmail.com>
2019-12-25 17:10:58 -0800

    change notifications a bit

--- src/screens/NotificationSettingsScreen.js
+++ src/screens/NotificationSettingsScreen.js
@@ -3,107 +3,69 @@ import {StyleSheet, View, Text} from 'react-native';
 
 import SwitchToggle from '@dooboo-ui/native-switch-toggle';
 
-// TODO: Better spacing
 export default class NotificationSettingsScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showNotifcations: false,
       showUpdates: false,
       showPromos: false,
     };
   }
   render() {
-    // Switch toggle needs a padding or else it breaks
     return (
       <View style={styles.container}>
-        <View style={styles.switchRowContainer}>
-          <View>
-            <Text style={styles.switchTitle}>Mute Notifications</Text>
-            <Text>{this.state.showNotifcations ? 'On' : 'Off'}</Text>
-          </View>
-          <SwitchToggle
-            switchOn={this.state.showNotifcations}
-            onPress={() =>
-              this.setState({showNotifcations: !this.state.showNotifcations})
-            }
-            circleColorOff={'white'}
-            circleColorOn={'black'}
-            backgroundColorOff={'lightgray'}
-            backgroundColorOn={'#F8B500'}
-            containerStyle={{
-              width: 80,
-              borderRadius: 30,
-              padding: 5,
-            }}
-          />
-        </View>
-
         <View style={{alignItems: 'center'}}>
-          <View style={styles.subSettingsContainer}>
-            <View
-              style={{
-                flexDirection: 'row',
-                paddingVertical: 10,
-                alignItems: 'center',
-              }}>
-              <View>
+          <View style={styles.settingsContainer}>
+            <View style={styles.rowContainer}>
+              <View style={styles.textContainer}>
                 <Text style={{fontSize: 20}}>Return Updates</Text>
                 <Text style={{color: 'gray'}}>
                   Keep updated with the status of your package
                 </Text>
               </View>
 
-              <SwitchToggle
-                switchOn={this.state.showUpdates}
-                onPress={() =>
-                  this.setState({
-                    showUpdates: !this.state.showUpdates,
-                  })
-                }
-                circleColorOff={'white'}
-                circleColorOn={'black'}
-                backgroundColorOff={'lightgray'}
-                backgroundColorOn={'#F8B500'}
-                containerStyle={{
-                  width: 60,
-                  borderRadius: 30,
-                  padding: 5,
-                }}
-              />
+              <View style={styles.switchView}>
+                <SwitchToggle
+                  disabled={!this.state.showNotifcations}
+                  switchOn={this.state.showUpdates}
+                  onPress={() =>
+                    this.setState({
+                      showUpdates: !this.state.showUpdates,
+                    })
+                  }
+                  circleColorOff={'white'}
+                  circleColorOn={'black'}
+                  backgroundColorOff={'lightgray'}
+                  backgroundColorOn={'#F8B500'}
+                  containerStyle={styles.switchContainer}
+                />
+              </View>
             </View>
 
-            <View
-              style={{
-                flexDirection: 'row',
-                paddingVertical: 10,
-                alignItems: 'center',
-              }}>
-              <View>
+            <View style={styles.rowContainer}>
+              <View style={styles.textContainer}>
                 <Text style={{fontSize: 20}}>Promotions and News</Text>
                 <Text style={{color: 'gray'}}>
                   Learn more about promotions and new features
                 </Text>
               </View>
-              
-              <SwitchToggle
-                switchOn={this.state.showPromos}
-                onPress={() =>
-                  this.setState({
-                    showPromos: !this.state.showPromos,
-                  })
-                }
-                circleColorOff={'white'}
-                circleColorOn={'black'}
-                backgroundColorOff={'lightgray'}
-                backgroundColorOn={'#F8B500'}
-                containerStyle={{
-                  width: 60,
-                  borderRadius: 30,
-                  padding: 5,
-                }}
-              />
 
+              <View style={styles.switchView}>
+                <SwitchToggle
+                  disabled={!this.state.showNotifcations}
+                  switchOn={this.state.showPromos}
+                  onPress={() =>
+                    this.setState({
+                      showPromos: !this.state.showPromos,
+                    })
+                  }
+                  circleColorOff={'white'}
+                  circleColorOn={'black'}
+                  backgroundColorOff={'lightgray'}
+                  backgroundColorOn={'#F8B500'}
+                  containerStyle={styles.switchContainer}
+                />
+              </View>
             </View>
           </View>
         </View>
@@ -116,22 +78,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  switchRowContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: 20,
-    borderTopWidth: 1,
-    borderBottomWidth: 1,
-    borderColor: 'lightgray',
+  switchContainer: {
+    width: 70,
+    borderRadius: 30,
+    padding: 5,
   },
-  switchTitle: {
-    fontSize: 20,
-  },
-  subSettingsContainer: {
+  settingsContainer: {
     width: '100%',
     borderRadius: 5,
     borderWidth: 1,
     borderColor: 'lightgray',
   },
+  rowContainer: {
+    flexDirection: 'row',
+    paddingVertical: 10,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  textContainer: {
+    paddingLeft: 10,
+  },
+  switchView: {
+    paddingRight: 10
+  }
 });
