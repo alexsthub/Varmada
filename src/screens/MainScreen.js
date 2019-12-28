9e0a8af30149ec7ef46c3c3573183b83e15aeeeb
dev <dev@Erickson-MacBookPro.local>
2019-12-28 00:54:57 -0800

    some bullshit work on MyPickupScreen

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {
+  ImageBackground,
+  Image,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Keyboard,
+} from 'react-native';
 import CustomButton from '../components/general/CustomButton';
 import LeftNavButton from '../components/leftNav/leftNavButton';
+
+import styles from '../constants/styles/loginStyles';
 
 // TODO: Make the button bigger. CustomButton needs to be more flexible.
 export default class MainScreen extends React.Component {
@@ -9,20 +20,28 @@ export default class MainScreen extends React.Component {
     super(props);
   }
 
-  handlePress = () => {
-    this.props.navigation.navigate('LoginScreen');
-  };
+  // handlePress = () => {
+  //   this.props.navigation.navigate('LoginScreen');
+  // };
   handlePickup = () => {
     this.props.navigation.navigate('PickupScreen');
   };
+  handleMyPickups = () => {
+    this.props.navigation.navigate('Pickups');
+  };
+
 
   //how do i space this text apart.
   render() {
     return (
+      <ImageBackground
+        source={require('../assets/loginGradient.jpg')}
+        style={styles.background}>
+
       <View style={{flex: 1}}>
         <LeftNavButton navigation={this.props.navigation} />
         <View style={{justifyContent: 'space-between'}}>
-          <Text style={{textAlign: 'center', fontSize: 30}}>Hi, Alex</Text>
+          <Text style={{textAlign: 'center', fontSize: 30}}>Sup Bitch</Text>
           <Text
             style={{
               textAlign: 'center',
@@ -31,16 +50,27 @@ export default class MainScreen extends React.Component {
             You don't have any pickups scheduled right now
           </Text>
         </View>
+        
+        <View style={styles.inputDivider}></View>
 
-        <View style={{flex: 1, flexDirection: 'column-reverse'}}>
           <CustomButton
             text={'Request a pickup!'}
             onPress={this.handlePickup}
             textStyle={{color: '#000000'}}
             buttonStyle={{elevation: 10}}
           />
-        </View>
+
+        <View style={styles.inputDivider}></View>
+
+          <CustomButton
+            text={'Pickup History'}
+            onPress={this.handleMyPickups}
+            textStyle={{color: '#000000'}}
+            buttonStyle={{elevation: 10}}
+          />
+
       </View>
+      </ImageBackground>
     );
   }
 }
--- src/screens/navscreens/MyPickupScreen.js
+++ src/screens/navscreens/MyPickupScreen.js
@@ -1,13 +1,72 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import NavScreenHeader from '../../components/general/NavScreenHeader';
+import {ImageBackground, FlatList, StyleSheet, View, Text} from 'react-native';
+// import CustomButton from '../components/general/CustomButton';
+// import CustomButton from '../components/general/CustomButton';
+// import LeftNavButton from '../components/leftNav/leftNavButton';
+// import NavScreenHeader from '../../components/general/NavScreenHeader';
+// import styles from '../constants/styles/loginStyles';
+import styles from '../../constants/styles/loginStyles';
 
 export default class MyPickupScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
-      <View>
-        <NavScreenHeader navigation={this.props.navigation} title={'My Pickups'}/>
+      <ImageBackground
+        source={require('../../assets/loginGradient.jpg')}
+        style={styles.background}>
+
+      <View style={stylesA.container}>
+        <FlatList
+          data={[
+            {key: 'Shoes'},
+            {key: 'Other Bullshit'},
+            {key: 'Toys'},
+          ]}
+          renderItem={({item}) => <Text style={stylesA.item}>{item.key}</Text>}
+        />
       </View>
+      </ImageBackground>
     );
   }
+  // render() {
+  //   return (
+  //     <View style={{flex: 1}}>
+  //       <Text> Hi </Text>
+  //       <LeftNavButton navigation={this.props.navigation} />
+  //     </View>
+  //   );
+  // }
 }
+
+const stylesA = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    color: 'white',
+  }
+})
+
+
+
+
+
+// import React from 'react';
+// import {StyleSheet, View, Text} from 'react-native';
+// import NavScreenHeader from '../../components/general/NavScreenHeader';
+
+// export default class MyPickupScreen extends React.Component {
+//   render() {
+//     return (
+//       <View>
+//         <NavScreenHeader navigation={this.props.navigation} title={'My Pickups'}/>
+//       </View>
+//     );
+//   }
+// }
